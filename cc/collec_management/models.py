1558dce9d2a2c503057df5444175df6ba794310c
Barhoumi Yusra <yusra.barhoumi@etu.univ-orleans.fr>
2024-10-30 00:50:36 +0100

    Ajout du modèle Collec

--- cc/collec_management/models.py
+++ cc/collec_management/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+import datetime;
+# Create your models here.
+class Collec(models.Model):
+
+    title = models.CharField(max_length=100, null=False)
+    description = models.TextField(null=False)
+    date=models.DateTimeField(default=datetime.date.today, null=False)
+
+    def __str__(self):
+        return self.title
