d8d3083b77e011b89c9d469619ad37481eef56f1
Barhoumi Yusra <yusra.barhoumi@etu.univ-orleans.fr>
2024-10-30 23:10:02 +0100

    q7

--- cc/cc/urls.py
+++ cc/cc/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
+from collec_management import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('about/', views.about)
+    path('about/', views.about),
+    path('new/', views.new_collection, name='new_collection'),
 ]
--- cc/collec_management/views.py
+++ cc/collec_management/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from .forms import CollecForm  
+from .models import Collec 
 # Create your views here.
+def about(request):
+    return render(request, "about.html")
+def new_collection(request):
+    if request.method == 'POST':
+        form = CollecForm(request.POST)
+        if form.is_valid():
+            collection = form.save(commit=False)
+            collection.date_creation = timezone.now()  
+            collection.save()
+            return redirect('about')  
+    else:
+        form = CollecForm()
+    return render(request, 'new_collection.html', {'form': form})
