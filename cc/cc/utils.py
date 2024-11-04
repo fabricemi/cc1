8b7ec39333f427348efb6681dd3a5a485625a28b
Missidi Mbazi Basseha Fabrice <fabrice.missidi-mbazi-basseha@etu.univ-orleans.fr>
2024-11-04 15:55:29 +0100

    suppresion du style bootstrap avant la q-10

--- cc/cc/utils.py
+++ cc/cc/utils.py
@@ -1,6 +1,7 @@
 
 
 def attr_class(form):
+    """attribut au champ input la casse 'form-control'"""
     for f in form.fields.keys():
         form.fields[f].widget.attrs.update({
             "class":"form-control"
