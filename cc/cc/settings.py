034f469e3d48f1117d10dab070881a398eed4b18
Missidi Mbazi Basseha Fabrice <fabrice.missidi-mbazi-basseha@etu.univ-orleans.fr>
2024-11-07 12:41:05 +0100

    erreu enleve

--- cc/cc/settings.py
+++ cc/cc/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'collec_management', 
-    'django_bootstrap5'
+    'collec_management'
 ]
 
 MIDDLEWARE = [
